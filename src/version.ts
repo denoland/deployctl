534d3dde87a95c7b3ee3c0990a9f24d9618b4d5a
Luca Casonato <lucacasonato@yahoo.com>
2021-04-08 13:20:26 +0200

    0.1.1

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const VERSION = "0.1.0";
+export const VERSION = "0.1.1";
 
 // The minium Deno version required. Currently 1.8 because we are making use of
 // the --unstable `deno info` output which changed in 1.8.
