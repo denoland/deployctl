bade1603f4707e8d84f90ffbd8df24b577399c04
Luca Casonato <lucacasonato@yahoo.com>
2021-04-22 21:43:11 +0200

    1.9.1

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 export const VERSION = "0.2.0";
 
-// The minium Deno version required. Currently 1.9 because we are making use of
-// the --unstable `Deno.startHttp` API.
-export const MINIMUM_DENO_VERSION = "1.9.0";
+// The minium Deno version required. Currently 1.9.1 because we are making use of
+// the --unstable `Deno.startHttp` API, and 1.9.0 still has some bugs.
+export const MINIMUM_DENO_VERSION = "1.9.1";
