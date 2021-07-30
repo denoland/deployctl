1e969d272d75c1546aaf807f20e2a366121a3e65
Yoshiya Hinosawa <stibium121@gmail.com>
2021-07-30 18:05:35 +0900

    chore: update std version

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.91.0/testing/asserts.ts";
-export { BufReader } from "https://deno.land/std@0.91.0/io/bufio.ts";
+export * from "https://deno.land/std@0.103.0/testing/asserts.ts";
+export { BufReader } from "https://deno.land/std@0.103.0/io/bufio.ts";
