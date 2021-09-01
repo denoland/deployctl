f851271301fe4db31a0818c26746ef3f3c5117c9
Luca Casonato <hello@lcas.dev>
2021-09-01 17:22:27 +0200

    refactor: dont use a worker for version check (#61)
    This means we don't need `--unstable` for the installation anymore.

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,3 @@
 export const VERSION = "0.3.0";
 
-// The minium Deno version required. Currently 1.9.1 because we are making use of
-// the --unstable `Deno.startHttp` API, and 1.9.0 still has some bugs.
-export const MINIMUM_DENO_VERSION = "1.9.1";
+export const MINIMUM_DENO_VERSION = "1.13.0";
