a534d90926862fc1eeb5b242a90c5971a8b832b7
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-16 09:39:39 +0800

    Delete examples directory

--- examples/serve_local.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2021 Deno Land Inc. All rights reserved. MIT license.
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(new URL("../tests/testdata/data.json", import.meta.url)),
-  );
-});
