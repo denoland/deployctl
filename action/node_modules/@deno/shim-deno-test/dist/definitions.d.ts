dd4628614abda725fe416c31a30b11a4a227d7c1
Luca Casonato <hello@lcas.dev>
2022-02-09 17:32:00 +0100

    check in node_modules

---
+++ action/node_modules/@deno/shim-deno-test/dist/definitions.d.ts
@@ -0,0 +1,3 @@
+import { TestDefinition } from "./deno.types.gen.js";
+/** Reference to the array that `Deno.test` calls insert their definition into. */
+export declare const testDefinitions: TestDefinition[];
