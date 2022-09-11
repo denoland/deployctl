a87c7c6767f44cae2cf5c72fd9867a67858e443e
Omar Diab <me@omardiab.com>
2022-09-11 20:17:06 +0900

    test console log

--- action/index.js
+++ action/index.js
@@ -20,6 +20,7 @@ async function main() {
   const importMap = core.getInput("import-map", {});
   const include = core.getInput("include", {})?.split(",").map(v => normalize(v));
   const exclude = core.getInput("exclude", {})?.split(",").map(v => normalize(v));
+  console.log({include, exclude})
   const cwd = resolve(process.cwd(), core.getInput("root", {}));
 
   if (github.context.eventName === "pull_request") {
