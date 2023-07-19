c8cab05b1525521f79b8ffc00875b4157cf3bcb3
Will (Newby) Atlas <willnewby@gmail.com>
2023-07-19 09:37:36 -0600

    Make DEPLOY_API_ENDPOINT more configurable (#166)
    * Change deployctl baked-in API path to allow for fully-configurable endpoints
    
    * Make default DEPLOY_API_ENDPOINTs include /api
    
    * Removing intermediate / during URL building

--- action/index.js
+++ action/index.js
@@ -11,7 +11,7 @@ import {
 } from "./deps.js";
 
 // The origin of the server to make Deploy requests to.
-const ORIGIN = process.env.DEPLOY_API_ENDPOINT ?? "https://dash.deno.com";
+const ORIGIN = process.env.DEPLOY_API_ENDPOINT ?? "https://dash.deno.com/api";
 
 async function main() {
   const projectId = core.getInput("project", { required: true });
