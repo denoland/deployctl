7c060a16c42777d91d069206c45fcf0f87770f1e
Arnau Orriols <arnauorriolsmiro@gmail.com>
2023-11-29 11:10:07 +0100

    fixes

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,4 +1,4 @@
 export { parseEntrypoint } from "./entrypoint.ts";
 export { API, APIError } from "./api.ts";
 export { walk } from "./walk.ts";
-export { fromFileUrl, normalize, resolve } from "../../deps.ts";
+export { fromFileUrl, resolve } from "../../deps.ts";
