b60fa9578b4130e0f429b6b263d3fc9091717b8e
Arnau Orriols <arnauorriolsmiro@gmail.com>
2023-11-29 12:56:06 +0100

    improve error message

--- src/utils/token_storage/fs.ts
+++ src/utils/token_storage/fs.ts
@@ -5,7 +5,7 @@ export async function get(): Promise<string | null> {
   try {
     const info = await Deno.lstat(credentialsPath);
     if (!info.isFile || (info.mode !== null && (info.mode & 0o777) !== 0o600)) {
-      throw new Error("The credentials file have have been tampered with.");
+      throw new Error("The credentials file has been tampered with and will be ignored. Please delete it.");
     }
   } catch (e) {
     if (e instanceof Deno.errors.NotFound) {
@@ -17,8 +17,8 @@ export async function get(): Promise<string | null> {
   try {
     const token = JSON.parse(await Deno.readTextFile(credentialsPath)).token;
     return token || null;
-  } catch (e) {
-    throw new Error(`The credentials file has been tampered with (${e}).`);
+  } catch (_) {
+    throw new Error(`The credentials file has been tampered with and will be ignored. Please delete it.`);
   }
 }
 
