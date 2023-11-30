a4be425e040b5c1f07b5fe53fca7baf821561eb6
Arnau Orriols <4871949+arnauorriols@users.noreply.github.com>
2023-11-30 10:22:33 +0100

    feat: support file storage for provisioned tokens in OSes != darwin (#202)

--- src/utils/token_storage.ts
+++ src/utils/token_storage.ts
@@ -14,12 +14,12 @@ if (Deno.build.os === "darwin") {
   const memory = await import("./token_storage/memory.ts");
   module = {
     get: defaultOnError(
-      "Failed to get token from Keychain",
+      "Failed to get token from Keychain. Will provision a new token for this execution but please make sure to fix the issue afterwards.",
       memory.get,
       darwin.getFromKeychain,
     ),
     store: defaultOnError(
-      "Failed to store token into Keychain",
+      "Failed to store token into Keychain. Will keep it in memory for the duration of this execution but please make sure to fix the issue afterwards.",
       memory.store,
       darwin.storeInKeyChain,
     ),
@@ -30,7 +30,25 @@ if (Deno.build.os === "darwin") {
     ),
   };
 } else {
-  module = await import("./token_storage/memory.ts");
+  const fs = await import("./token_storage/fs.ts");
+  const memory = await import("./token_storage/memory.ts");
+  module = {
+    get: defaultOnError(
+      "Failed to get token from credentials file. Will provision a new token for this execution but please make sure to fix the issue afterwards.",
+      memory.get,
+      fs.get,
+    ),
+    store: defaultOnError(
+      "Failed to store token in credentials file. Will keep it in memory for the duration of this execution but please make sure to fix the issue afterwards.",
+      memory.store,
+      fs.store,
+    ),
+    remove: defaultOnError(
+      "Failed to remove token from credentials file",
+      memory.remove,
+      fs.remove,
+    ),
+  };
 }
 export default module;
 
@@ -50,9 +68,9 @@ function defaultOnError<
         .catch((err) => {
           const spinnerInterrupt = interruptSpinner();
           wait("").start().warn(notification);
-          let errStr = err.toString();
-          if (errStr.length > 80) {
-            errStr = errStr.slice(0, 80) + "...";
+          let errStr = err.message;
+          if (errStr.length > 90) {
+            errStr = errStr.slice(0, 90) + "...";
           }
           wait({ text: "", indent: 3 }).start().fail(errStr);
           spinnerInterrupt.resume();
