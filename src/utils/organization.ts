2cb039ec2c3388085ce9bc6916f98a8e0dfe3f44
Arnau Orriols <arnauorriolsmiro@gmail.com>
2023-12-18 14:28:00 +0100

    feat: Support creating projects to orgs other than the personal org

---
+++ src/utils/organization.ts
@@ -0,0 +1,34 @@
+import { error } from "../error.ts";
+import { API } from "./api.ts";
+import { Organization } from "./api_types.ts";
+import { interruptSpinner, wait } from "./spinner.ts";
+
+export default {
+  getByNameOrCreate: async (
+    api: API,
+    name: string,
+  ): Promise<Organization> => {
+    const interruptedSpinner = interruptSpinner();
+    let org;
+    try {
+      let spinner = wait(
+        `You have specified the organization ${name}. Fetching details...`,
+      ).start();
+      org = await api.getOrganizationByName(name);
+      if (!org) {
+        spinner.stop();
+        spinner = wait(
+          `Organization '${name}' not found. Creating...`,
+        ).start();
+        org = await api.createOrganization(name);
+        spinner.succeed(`Created new organization '${org!.name}'.`);
+      } else {
+        spinner.stop();
+      }
+    } catch (e) {
+      error(e.message);
+    }
+    interruptedSpinner.resume();
+    return org;
+  },
+};
