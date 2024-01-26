f21311b48f62f0063e3c729fc8a6a5dcdc4da7f9
Arnau Orriols <4871949+arnauorriols@users.noreply.github.com>
2024-01-26 02:22:15 +0100

    fix: 1.40 deprecation warnings (#246)

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,4 +1,16 @@
+import { semverGreaterThanOrEquals } from "../../deps.ts";
+
 export { parseEntrypoint } from "./entrypoint.ts";
 export { API, APIError } from "./api.ts";
 export { walk } from "./walk.ts";
 export { fromFileUrl, resolve } from "../../deps.ts";
+
+// deno-lint-ignore no-explicit-any
+export function isTerminal(stream: any) {
+  if (semverGreaterThanOrEquals(Deno.version.deno, "1.40.0")) {
+    return stream.isTerminal();
+  } else {
+    // deno-lint-ignore no-deprecated-deno-api
+    return Deno.isatty(stream.rid);
+  }
+}
