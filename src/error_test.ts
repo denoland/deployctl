04747d3e3bf3c2168bb05077eef2872752111480
Yusuke Tanaka <wing0920@gmail.com>
2024-10-18 13:25:52 +0900

    format

--- src/error_test.ts
+++ src/error_test.ts
@@ -4,48 +4,48 @@ import { stringify } from "./error.ts";
 import { assert, assertEquals } from "@std/assert";
 
 Deno.test("stringify string", () => {
-    assertEquals(stringify("test"), "test");
+  assertEquals(stringify("test"), "test");
 });
 
 Deno.test("stringify number", () => {
-    assertEquals(stringify(42), "42");
+  assertEquals(stringify(42), "42");
 });
 
 Deno.test("stringify object", () => {
-    assertEquals(stringify({ foo: 42 }), `{\n  "foo": 42\n}`);
+  assertEquals(stringify({ foo: 42 }), `{\n  "foo": 42\n}`);
 });
 
 Deno.test("stringify Error", () => {
-    const got = stringify(new Error("boom"));
-    assert(got.startsWith("Error: boom\n"), `assert failed: ${got}`);
+  const got = stringify(new Error("boom"));
+  assert(got.startsWith("Error: boom\n"), `assert failed: ${got}`);
 });
 
 Deno.test("stringify Error with cause (cause is also Error)", () => {
-    const e1 = new TypeError("e1");
-    const e2 = new SyntaxError("e2", { cause: e1 });
-    const got = stringify(e2);
+  const e1 = new TypeError("e1");
+  const e2 = new SyntaxError("e2", { cause: e1 });
+  const got = stringify(e2);
 
-    assert(
-        got.startsWith("SyntaxError: e2\n"),
-        `assert failed: ${got}`,
-    );
-    assert(
-        got.includes("Caused by TypeError: e1\n"),
-        `assert failed: ${got}`,
-    );
+  assert(
+    got.startsWith("SyntaxError: e2\n"),
+    `assert failed: ${got}`,
+  );
+  assert(
+    got.includes("Caused by TypeError: e1\n"),
+    `assert failed: ${got}`,
+  );
 });
 
 Deno.test("stringify Error with cause (cause is number)", () => {
-    const e = new Error("e", { cause: 42 });
-    const got = stringify(e);
+  const e = new Error("e", { cause: 42 });
+  const got = stringify(e);
 
-    assert(
-        got.startsWith("Error: e\n"),
-        `assert failed: ${got}`,
-    );
+  assert(
+    got.startsWith("Error: e\n"),
+    `assert failed: ${got}`,
+  );
 
-    assert(
-        got.endsWith("Caused by 42"),
-        `assert failed: ${got}`,
-    );
+  assert(
+    got.endsWith("Caused by 42"),
+    `assert failed: ${got}`,
+  );
 });
