3139152f18500bb305b7966a8fc68dff5491d3f6
Yusuke Tanaka <wing0920@gmail.com>
2024-10-18 14:13:10 +0900

    force @deno/emit to use @deno/cache-dir@0.13.0 to get bundling to work

--- tools/bundle.ts
+++ tools/bundle.ts
@@ -1,12 +1,16 @@
 // Copyright 2024 Deno Land Inc. All rights reserved. MIT license.
 
-import { bundle } from "@deno/emit";
+import { bundle, type ImportMap } from "@deno/emit";
 import { resolve } from "@std/path/resolve";
+import { parse as parseJsonc } from "@std/jsonc";
 
 const entrypoint = Deno.args[0];
 const resolvedPath = resolve(Deno.cwd(), entrypoint);
+
+const configPath = resolve(Deno.cwd(), "deno.jsonc");
+const config = await Deno.readTextFile(configPath);
 const result = await bundle(resolvedPath, {
-  importMap: "deno.json",
+  importMap: parseJsonc(config) as ImportMap,
 });
 console.log(`// deno-fmt-ignore-file
 // deno-lint-ignore-file
