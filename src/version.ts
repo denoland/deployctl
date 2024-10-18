70b0fc8fd82b9fbd003b712d60f03509e7c06ea3
Yusuke Tanaka <wing0920@gmail.com>
2024-10-18 14:50:54 +0900

    try to decrease minimum deno version to 1.46.0 again

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 export const VERSION = "1.12.0";
 
 // Make sure to keep this in sync with the "old" version in `ci.yml`
-export const MINIMUM_DENO_VERSION = "2.0.0";
+export const MINIMUM_DENO_VERSION = "1.46.0";
