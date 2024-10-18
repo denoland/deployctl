0bc0ed326672063f8ee82ae52f84bf6a26af8145
Yusuke Tanaka <yusuktan@maguro.dev>
2024-10-18 20:04:53 +0900

    update minimum required Deno version in README.md

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,5 @@
 export const VERSION = "1.12.0";
 
 // Make sure to keep this in sync with the "old" version in `ci.yml`
+// Also don't forget to update README.md.
 export const MINIMUM_DENO_VERSION = "1.46.0";
