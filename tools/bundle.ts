0b962cadf826ea8e930806dafb2709cda2bab8ca
Yusuke Tanaka <wing0920@gmail.com>
2024-10-18 13:31:01 +0900

    fix bundle.ts

--- tools/bundle.ts
+++ tools/bundle.ts
@@ -1,11 +1,13 @@
 // Copyright 2024 Deno Land Inc. All rights reserved. MIT license.
 
-import { bundle } from "jsr:@deno/emit@0.43.1";
-import { resolve } from "jsr:@std/path@1.0.1/resolve";
+import { bundle } from "@deno/emit";
+import { resolve } from "@std/path/resolve";
 
 const entrypoint = Deno.args[0];
 const resolvedPath = resolve(Deno.cwd(), entrypoint);
-const result = await bundle(resolvedPath);
+const result = await bundle(resolvedPath, {
+  importMap: "deno.json",
+});
 console.log(`// deno-fmt-ignore-file
 // deno-lint-ignore-file
 // This code was bundled using \`deno task build-action\` and it's not recommended to edit it manually
