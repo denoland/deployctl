440869a9beba44cac6bf763193f145a53ab67296
Yusuke Tanaka <wing0920@gmail.com>
2024-10-29 16:58:10 +0900

    export fromFileUrl and resolve

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,3 +1,5 @@
+// Export functions used by `action/index.js`
 export { parseEntrypoint } from "./entrypoint.ts";
 export { API, APIError } from "./api.ts";
 export { convertPatternToRegExp, walk } from "./manifest.ts";
+export { fromFileUrl, resolve } from "@std/path";
