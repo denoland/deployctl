5e22144f5f3628d03841eff0687e1d3f7b856988
uncenter <47499684+uncenter@users.noreply.github.com>
2025-02-05 09:35:13 -0500

    fix: respect DENO_DIR and XDG_* environment variables for fs cache

--- src/utils/info.ts
+++ src/utils/info.ts
@@ -5,12 +5,24 @@ export function getConfigPaths() {
   const homeDir = Deno.build.os == "windows"
     ? Deno.env.get("USERPROFILE")!
     : Deno.env.get("HOME")!;
-  const configDir = join(homeDir, ".deno", "deployctl");
+  const xdgCacheDir = Deno.env.get("XDG_CACHE_HOME");
+  const xdgConfigDir = Deno.env.get("XDG_CONFIG_HOME");
+
+  const denoDir = Deno.env.get("DENO_DIR") || join(homeDir, ".deno");
+  const cacheDir = join(
+    xdgCacheDir ? join(xdgCacheDir, "deno") : denoDir,
+    "deployctl",
+  );
+  const configDir = join(
+    xdgConfigDir ? join(xdgConfigDir, "deno") : denoDir,
+    "deployctl",
+  );
 
   return {
+    cacheDir,
     configDir,
-    updatePath: join(configDir, "update.json"),
-    credentialsPath: join(configDir, "credentials.json"),
+    updatePath: join(cacheDir, "update.json"),
+    credentialsPath: join(cacheDir, "credentials.json"),
   };
 }
 
@@ -18,8 +30,8 @@ export async function fetchReleases() {
   try {
     const { latest } = await getVersions();
     const updateInfo = { lastFetched: Date.now(), latest };
-    const { updatePath, configDir } = getConfigPaths();
-    await Deno.mkdir(configDir, { recursive: true });
+    const { updatePath, cacheDir } = getConfigPaths();
+    await Deno.mkdir(cacheDir, { recursive: true });
     await Deno.writeFile(
       updatePath,
       new TextEncoder().encode(JSON.stringify(updateInfo, null, 2)),
